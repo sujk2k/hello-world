00291e783addd79dd3acb29af59713f37a672ae8
sujk2k_ubuntu <sujeet.kudale@hotmail.com>
2017-05-21 17:27:49 +0530

    C# hello world app

---
+++ hwCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hwCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name= System.Environment.MachineName;
+            Console.WriteLine("Hello World! on {0}", name);
+        }
+    }
+}
