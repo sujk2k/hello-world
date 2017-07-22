2503b077997484c5b19fdd6c9686502f3ed41a3f
sujk2k <sujk2k@hotmail.com>
2017-07-22 22:34:42 +0530

    sqlvscodescripboard with docker sql profile connection string added

---
+++ sqlvscodescripboard.sql
@@ -0,0 +1,14 @@
+/*
+"mssql.connections": [
+        {
+            "server": "localhost",
+            "database": "sujk2kdb",
+            "authenticationType": "SqlLogin",
+            "user": "sa",
+            "password": "",
+            "emptyPasswordInput": false,
+            "savePassword": true,
+            "profileName": "sqldocker"
+        }
+    ]
+    */
