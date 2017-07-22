8b43af269458fbd8b225685ee34f78d62aa29668
sujk2k <sujk2k@hotmail.com>
2017-07-22 22:43:03 +0530

    docker command added

--- sqlvscodescripboard.sql
+++ sqlvscodescripboard.sql
@@ -1,7 +1,10 @@
 -- start docker container for ms sql
+-- docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Passw0rd' -p 1433:1433 -d --name juliesqllinux microsoft/mssql-server-linux
+
+-- start sqlcmd shell
 -- ./sqlcmd -S localhost -U sa  -P Passw0rdD
 
--- local mssql docker connection string
+-- local mssql docker connection string in vscode
 /*
 "mssql.connections": [
         {
