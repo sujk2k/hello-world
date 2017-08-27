e118e4763e8e5f2ac801888e5b0791e4f197f27a
sujk2k_dell <sujeet.kudale@hotmail.com>
2017-08-27 11:43:30 +0530

    WPF simple client created to use CXF web service

---
+++ CxFWPFClient/CxFWPFClient/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CxFWPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        JavaCxFReference.HelloWorldClient helloWorldClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            helloWorldClient = new JavaCxFReference.HelloWorldClient();
+        }
+
+        private void btnProcessRequest_Click(object sender, RoutedEventArgs e)
+        {
+            int result = helloWorldClient.addMyTwoNumbers(int.Parse(txtNumber1.Text), int.Parse(txtNumber2.Text));
+            txtResult.Text = result.ToString();
+        }
+    }
+}
