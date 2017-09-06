20b6f68fef7c5044f35431860903674767c84477
sujk2k_dell <sujeet.kudale@hotmail.com>
2017-09-06 17:32:55 +0530

    .NET core standalone deployment test project added

---
+++ hwDotNetCore/hwDotNetCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hwDotNetCore
+{
+    class Program
+    {
+        /// <summary>
+        /// Self contained deployment project. Note changes in .csproj file to enable this
+        /// https://docs.microsoft.com/en-us/dotnet/core/deploying/deploy-with-vs
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
