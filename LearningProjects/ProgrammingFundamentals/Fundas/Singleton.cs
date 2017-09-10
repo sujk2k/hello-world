06be84b8128598c368f1c487ddf0a8b9abe572dc
sujk2k_dell <sujeet.kudale@hotmail.com>
2017-09-10 22:58:17 +0530

    Singleton implementations added

--- LearningProjects/ProgrammingFundamentals/Fundas/Singleton.cs
+++ LearningProjects/ProgrammingFundamentals/Fundas/Singleton.cs
@@ -4,18 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ProgrammingFundamentals;
+using System.Threading;
 
 namespace ProgrammingFundamentals.Fundas
 {
-
     public class SingletonClient : IExcecuteClient
     {
         public void ExecuteClient()
         {
-            // Try to create 3 instances from Singleton class
-            Singleton instance1 = Singleton.SingleInsance;
-            Singleton instance2 = Singleton.SingleInsance;
-            Singleton instance3 = Singleton.SingleInsance;
+            // Client to test Singleton implementation with Lazy instantiation and with NO thread safety
+            ExecuteSingletonNoThreadSafe();
+
+            //Client to test Singleton implementation with Lazy instantiation and WITH thread safety
+            ExecuteSingletonThreadSafe();
+        }
+
+
+        private void ExecuteSingletonThreadSafe()
+        {
+            Thread th1 = new Thread(delegate () { SingletonThreadSafe.GetInstance(); });
+            Thread th2 = new Thread(delegate () { SingletonThreadSafe.GetInstance(); });
+            th1.Start();
+            th2.Start();
+        }
+
+        private static void ExecuteSingletonNoThreadSafe()
+        {
+            // Try to create 3 instances from SingletonNoThreadSafe class
+            SingletonNoThreadSafe instance1 = SingletonNoThreadSafe.SingleInsance;
+            SingletonNoThreadSafe instance2 = SingletonNoThreadSafe.SingleInsance;
+            SingletonNoThreadSafe instance3 = SingletonNoThreadSafe.SingleInsance;
 
             // Display WorkerMethod output from Singleton
             Console.WriteLine(instance1.WorkerMethod());
@@ -23,28 +41,31 @@ namespace ProgrammingFundamentals.Fundas
             Console.WriteLine(instance3.WorkerMethod());
         }
     }
-
-    public class Singleton
+    /// <summary>
+    /// Below implementation is not thread safe as two threads may create 2 separate instances in case
+    /// </summary>
+    public class SingletonNoThreadSafe
     {
         // instance variable to prove that only one instance is being used
         int callerCount = 1;
 
-        // Private constructro to avoid initialization of class from outside clients
-        private Singleton()
+        // Private constructor to avoid initialization of class from outside clients
+        private SingletonNoThreadSafe()
         {
         }
 
         // Static single instance variable to hold insance of the class
-        private static Singleton singleInstance = null;
+        private static SingletonNoThreadSafe singleInstance = null;
 
         // Lazy instantiation. New single instance will created only when class property is referenced
-        public static Singleton SingleInsance
+        // This is not thread safe implementation as 2 same threads can evaluate false condition and create multiple instances
+        public static SingletonNoThreadSafe SingleInsance
         {
             get
             {
                 if (singleInstance == null)
                 {
-                    singleInstance = new Singleton();
+                    singleInstance = new SingletonNoThreadSafe();
                 }
                 return singleInstance;
             }
@@ -54,8 +75,36 @@ namespace ProgrammingFundamentals.Fundas
         // via incrementing and returning same callerCount variable on each instance call
         public string WorkerMethod()
         {
-            String str = "Hello from WorkerMethod " + (callerCount++) + " time";
+            String str = "Hello from SingletonNoThreadSafe WorkerMethod " + (callerCount++) + " time";
             return str;
+        }
+    }
+
+    /// <summary>
+    /// Thread safe implementation of Singleton
+    /// Lock avoids multiple threads simultaniously accessing multiple instances using Lock
+    /// </summary>
+    public class SingletonThreadSafe
+    {
+        private SingletonThreadSafe()
+        {
+
+        }
+
+        private static SingletonThreadSafe instance = null;
+
+        private static readonly object padLock = new object();
+
+        public static SingletonThreadSafe GetInstance()
+        {
+            lock (padLock)
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonThreadSafe();
+                }
+                return instance;
+            }
         }
     }
 }
