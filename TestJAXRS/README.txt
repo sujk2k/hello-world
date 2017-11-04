4f7e4dd05623cf51375035e1d6876ceeefa41ac2
sujk2k <sherlockbourne@hotmail.com>
2017-11-04 21:13:27 +0530

    sample jax-rs test project added

---
+++ TestJAXRS/README.txt
@@ -0,0 +1,2 @@
+Reference
+https://www.acando.no/thedailypassion/200555/a-rest-service-with-jetty-and-jersey
---
+++ TestJAXRS/src/main/java/org/sujk2k/personal/App.java
@@ -0,0 +1,30 @@
+package org.sujk2k.personal;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class App {
+    public static void main(String[] args) {
+        ResourceConfig config = new ResourceConfig();
+        config.packages("org.sujk2k.personal");
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+        Server server = new Server(2222);
+        ServletContextHandler context = new ServletContextHandler(server, "/*");
+        context.addServlet(servlet, "/*");
+
+        try {
+            server.start();
+            server.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            server.destroy();
+        }
+    }
+}
