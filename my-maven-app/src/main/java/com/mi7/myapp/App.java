21a857cf8592b22525602d8a2dd29d197aaa37f5
Sujeet Kudale <sujeet.kudale@hotmail.com>
2018-03-25 11:48:52 +0530

    Visual studio code sample projects added my jetty app, my node express app, my maven app, my spring boot sample repo

---
+++ my-maven-app/src/main/java/com/mi7/myapp/App.java
@@ -0,0 +1,13 @@
+package com.mi7.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
