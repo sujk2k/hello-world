cc5861274bd78c1fb1af7543645b8a9beb53ba0f
Sujeet Kudale <sujeet.kudale@hotmail.com>
2018-05-12 12:29:05 +0530

    Added first android hello world program

---
+++ head-first-android/HelloWorldAndroid/app/src/main/java/com/sujk2k/hello/helloworldandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sujk2k.hello.helloworldandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
