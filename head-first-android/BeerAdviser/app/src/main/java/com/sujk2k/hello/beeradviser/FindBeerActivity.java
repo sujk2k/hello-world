13c5d044b8a76403d648889ed345473ff57e2ab7
Sujeet Kudale <sujeet.kudale@hotmail.com>
2018-05-12 16:59:37 +0530

    Find beer event handling

--- head-first-android/BeerAdviser/app/src/main/java/com/sujk2k/hello/beeradviser/FindBeerActivity.java
+++ head-first-android/BeerAdviser/app/src/main/java/com/sujk2k/hello/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.sujk2k.hello.beeradviser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
 
@@ -9,5 +12,20 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    // called when button gets clicked
+    public void onClickFindBeer(View view){
+        // get reference to the textView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        // get the reference to the spinner
+        Spinner colors = (Spinner) findViewById(R.id.color);
+
+        // get selected item in the spinner
+        String beerType = String.valueOf(colors.getSelectedItem());
+
+        // display the selected item
+        brands.setText(beerType);
     }
 }
