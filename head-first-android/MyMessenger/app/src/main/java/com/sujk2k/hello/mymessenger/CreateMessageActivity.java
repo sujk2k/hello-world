b4dd3962470261c10936256e8a5b34e29e8395e9
Sujeet Kudale <sujeet.kudale@hotmail.com>
2018-05-28 14:12:35 +0530

    MyMessenger project - added implicit intent

--- head-first-android/MyMessenger/app/src/main/java/com/sujk2k/hello/mymessenger/CreateMessageActivity.java
+++ head-first-android/MyMessenger/app/src/main/java/com/sujk2k/hello/mymessenger/CreateMessageActivity.java
@@ -21,11 +21,29 @@ public class CreateMessageActivity extends Activity {
         // get value from EditText control
         String messageText = editText.getText().toString();
 
+        // Explicit intent: Calling another intent in application
         // state intent to call another activity
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
 
         // add message text to intent to pass on further
         intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+
+        // start another activity
+        startActivity(intent);
+    }
+
+    public void onSendMessageImplicit(View view){
+        // get EditText control
+        EditText editText = (EditText) findViewById(R.id.message);
+
+        // get value from EditText control
+        String messageText = editText.getText().toString();
+
+        // Implicit intent: calling actions via intent on android platform
+        // Below will provide list of all activities present on device capable of handling send message e.g. gmail, messenger etc.
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
 
         // start another activity
         startActivity(intent);
