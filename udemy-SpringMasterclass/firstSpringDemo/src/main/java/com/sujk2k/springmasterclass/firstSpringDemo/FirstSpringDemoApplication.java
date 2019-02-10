e6419bbfeb574a2b4007c67043522bbc70adc2aa
sujk2k <sherlockbourne@hotmail.com>
2019-02-10 19:34:08 +0530

    udemy-springmasterclass - first spring boot project

---
+++ udemy-SpringMasterclass/firstSpringDemo/src/main/java/com/sujk2k/springmasterclass/firstSpringDemo/FirstSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package com.sujk2k.springmasterclass.firstSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringDemoApplication.class, args);
+	}
+
+}
+
---
+++ udemy-SpringMasterclass/firstSpringDemo/src/test/java/com/sujk2k/springmasterclass/firstSpringDemo/FirstSpringDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sujk2k.springmasterclass.firstSpringDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstSpringDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
