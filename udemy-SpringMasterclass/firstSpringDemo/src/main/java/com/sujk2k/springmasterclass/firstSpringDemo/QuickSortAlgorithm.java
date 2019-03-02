6f2f9837ddf522abba94e4efe4245356832d6a26
sujk2k <sujeet.kudale@hotmail.com>
2019-03-02 12:33:33 +0530

    @Primary annotation related comment added

--- udemy-SpringMasterclass/firstSpringDemo/src/main/java/com/sujk2k/springmasterclass/firstSpringDemo/QuickSortAlgorithm.java
+++ udemy-SpringMasterclass/firstSpringDemo/src/main/java/com/sujk2k/springmasterclass/firstSpringDemo/QuickSortAlgorithm.java
@@ -3,6 +3,8 @@ package com.sujk2k.springmasterclass.firstSpringDemo;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+// @Primary annotation tells spring to give preference to this bean since more than one Sortalgorithm 
+// implementation beans are present in this spring context
 @Component
 @Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
