d4100de92b2317606e5dbb9169115066d0f3474f
sujk2k <sujeet.kudale@hotmail.com>
2019-10-19 11:42:01 +0530

    First Angular 8 app created

---
+++ Domain Driven Design/DDLInPractice/DDLInPractice.Logic/Money.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDLInPractice.Logic
+{
+    public class Money
+    {
+        public int OneCentCount { get;  set; }
+        public int TenCentCount { get;  set; }
+        public int QuarterCount { get;  set; }
+        public int OneDollarCount { get;  set; }
+        public int FiveDollarCount { get;  set; }
+        public int TwentyDollarCount { get;  set; }
+
+        public Money(
+            int oneCentCount,
+            int tencentcount,
+            int quarterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount)
+        {
+            OneCentCount = oneCentCount;
+            tencentcount = TenCentCount;
+            quarterCount = QuarterCount;
+            oneDollarCount = OneDollarCount;
+            FiveDollarCount = fiveDollarCount;
+            TwentyDollarCount = twentyDollarCount;
+        }
+
+        public static Money operator + (Money money1, Money money2)
+        {
+            Money sum = new Money(
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCount + money2.QuarterCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount);
+
+            return sum;
+        }
+    }
+}
---
+++ Domain Driven Design/DDLInPractice/DDLInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDLInPractice.Logic
+{
+    public sealed class SnackMachine : Entity
+    {
+        public Money MoneyInside { get; private set; }
+        public Money MoneyInTransaction { get; private set; }
+
+        public void InsertMoney(
+            Money money)
+        {
+            MoneyInTransaction += money;
+        }
+
+        public void ReturnMoney()
+        {
+            //MoneyInTransaction = 0;
+        }
+
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransaction;
+
+            //MoneyInTransaction = 0;
+        }
+    }
+}
