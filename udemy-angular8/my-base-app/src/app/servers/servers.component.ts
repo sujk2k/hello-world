78bc2ceb53c0aaa8a26865434ec6b36b5b2735b6
sujk2k <sujeet.kudale@hotmail.com>
2019-10-19 19:02:04 +0530

    Selector definition options - element, attribute, class

--- udemy-angular8/my-base-app/src/app/servers/servers.component.ts
+++ udemy-angular8/my-base-app/src/app/servers/servers.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // Selector options
+  // 1. Selector as a element
   selector: 'app-servers',
+
+  // 2. Selector as a attribute
+  //selector: '[app-servers]',
+
+  // 3. Selector as a class
+  //selector: '.app-servers',
+
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
