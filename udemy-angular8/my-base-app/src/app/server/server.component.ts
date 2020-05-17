bb182222060113af514c979946067dee0cc05a84
Kudale <182433@cognizant.com>
2020-05-17 17:30:01 +0530

    angular 8 - property binding added

--- udemy-angular8/my-base-app/src/app/server/server.component.ts
+++ udemy-angular8/my-base-app/src/app/server/server.component.ts
@@ -7,8 +7,16 @@ import { Component } from "@angular/core";
 export class ServerComponent{
     serverId: number=10;
     serverStatus: string = 'Offline';
+    serverRented: boolean = false;
 
     getServerStatus(){
         return this.serverStatus;
+    }
+
+    getServerRentedStatus(){
+        if (this.serverRented)
+            return 'Rented';
+        else
+            return 'Not Rented';
     }
 }
